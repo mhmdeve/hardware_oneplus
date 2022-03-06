1908d1e146cd36b213c22c41a989709dc2de9bcb
Master <mhmdeveloperofficial@gmail.com>
2022-03-06 18:13:58 +0500

    Revert "KeyHandler: adapt to fwb changes"
    This reverts commit 7e3bcb5f0e239288b0e992cfc8e10674a009102d.

--- KeyHandler/src/org/lineageos/settings/device/KeyHandler.kt
+++ KeyHandler/src/org/lineageos/settings/device/KeyHandler.kt
@@ -16,26 +16,23 @@ class KeyHandler(context: Context) : DeviceKeyHandler {
     private val audioManager = context.getSystemService(AudioManager::class.java)
     private val vibrator = context.getSystemService(Vibrator::class.java)
 
-    override fun handleKeyEvent(event: KeyEvent): Boolean {
+    override fun handleKeyEvent(event: KeyEvent): KeyEvent {
         if (event.action == KeyEvent.ACTION_DOWN) {
             when (event.scanCode) {
                 MODE_NORMAL -> {
                     audioManager.setRingerModeInternal(AudioManager.RINGER_MODE_NORMAL)
                     vibrator.vibrate(MODE_NORMAL_EFFECT)
-                    return true
                 }
                 MODE_VIBRATION -> {
                     audioManager.setRingerModeInternal(AudioManager.RINGER_MODE_VIBRATE)
                     vibrator.vibrate(MODE_VIBRATION_EFFECT)
-                    return true
                 }
                 MODE_SILENCE -> {
                     audioManager.setRingerModeInternal(AudioManager.RINGER_MODE_SILENT)
-                    return true
                 }
             }
         }
-        return false
+        return event
     }
 
     companion object {
