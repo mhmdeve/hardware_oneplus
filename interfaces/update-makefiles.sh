arcana-ext/platform_vendor_aosp

Add charge control HAL
Change-Id: I212501f4c93b875c8a1c280d17c319a5fb8e303f
Signed-off-by: LibXZR <i@xzr.moe>
Signed-off-by: dlwlrma123 <alexfinhart@gmail.com>

interfaces/update-makefiles.sh
@@ -1,0 +1,6 @@
+#!/bin/bash
+
+source $ANDROID_BUILD_TOP/system/tools/hidl/update-makefiles-helper.sh
+
+do_makefiles_update \
+  "vendor.kscope:vendor/aosp/interfaces"

mhmdeve/hardware_oneplus

Signed-off-by: Master <mhmdeveloperofficial@gmail.com>

interfaces/update-makefiles.sh
@@ -7,3 +7,6 @@ do_makefiles_update \
 
 do_makefiles_update \
   "vendor.oneplus:hardware/oneplus/interfaces/oneplus"
+
+do_makefiles_update \
+  "vendor.kscope:hardware/oneplus/interfaces"